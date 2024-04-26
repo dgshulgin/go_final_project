5b1883b1d9fa1a520b6a0a5c38be48646fff83e5
Dmitriy Shulgin <dshulgin@mail.ru>
2024-04-26 15:37:01 +0300

    nextdate is ready

--- handler/handler.go
+++ handler/handler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"net/http"
 
+	"github.com/dgshulgin/go_final_project/internal/pkg/nextdate"
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 )
@@ -14,6 +15,32 @@ const (
 
 func Router(log logrus.FieldLogger) (*mux.Router, error) {
 	router := mux.NewRouter()
+
+	router.HandleFunc("/api/nextdate", func(resp http.ResponseWriter, req *http.Request) {
+		start := req.FormValue("date")
+		now := req.FormValue("now")
+		repeat := req.FormValue("repeat")
+		log.Printf("api/nextdate, start=%s, now=%s, repeat={%s}", start, now, repeat)
+		if len(repeat) == 0 {
+			resp.WriteHeader(http.StatusOK)
+			resp.Write([]byte{})
+			log.Printf("отправлена пустая строка") // по логике, тако таск должен удаляться
+			return
+		}
+		nextDate, err := nextdate.NextDate(start, now, repeat)
+		if err != nil {
+			log.Printf("ошибка, %q", err)
+			resp.WriteHeader(http.StatusBadRequest)
+			resp.Write([]byte(err.Error()))
+			return
+		}
+		resp.WriteHeader(http.StatusOK)
+		resp.Write([]byte(nextDate))
+		log.Printf("отправлено, %q", nextDate)
+	}).Methods(http.MethodGet)
+
+	// я должен быть последним
 	router.PathPrefix("/").Handler(http.FileServer(http.Dir(webDir)))
+
 	return router, nil
 }
---
+++ internal/pkg/nextdate/nextdate.go
@@ -0,0 +1,88 @@
+package nextdate
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+	"unicode"
+)
+
+const (
+	maxDays        = 400
+	formatDateTime = "20060102"
+)
+
+type Handler func(start string, params []string) (string, error)
+
+// допустимо только одно значение < 400
+func repeatInDays(start string, params []string) (string, error) {
+	if len(params) == 0 {
+		return "", fmt.Errorf("отсутствует значение для ключа d")
+	}
+	days, err := strconv.Atoi(params[0])
+	if err != nil {
+		return "", fmt.Errorf("некорректное значение аргумента для ключа d: %s", params[0])
+	}
+	if max(days, maxDays) == days {
+		return "", fmt.Errorf("значение аргумента для ключа d превышает 400")
+	}
+	start0, err := time.Parse(formatDateTime, start)
+	if err != nil {
+		return "", fmt.Errorf("ошибка преобразования даты %s", start)
+	}
+	nextTime := start0.AddDate(0, 0, days)
+	return nextTime.Format(formatDateTime), nil
+}
+
+func repeatInYears(start string, params []string) (string, error) {
+	start0, err := time.Parse(formatDateTime, start)
+	if err != nil {
+		return "", fmt.Errorf("ошибка преобразования даты %s", start)
+	}
+	nextTime := start0.AddDate(1, 0, 0)
+	return nextTime.Format(formatDateTime), nil
+
+}
+
+func repeatInWeeks(start string, params []string) (string, error) {
+	return "", fmt.Errorf("неподдерживаемый формат w")
+}
+
+func repeatInMonths(start string, params []string) (string, error) {
+	return "", fmt.Errorf("неподдерживаемый формат m")
+}
+
+var ruler = map[string]Handler{
+	"d": repeatInDays,
+	"y": repeatInYears,
+	"w": repeatInWeeks,
+	"m": repeatInMonths,
+}
+
+// Ожидает непустой repeat.
+func NextDate(start string, now string, repeat string) (string, error) {
+	rep := strings.FieldsFunc(repeat, func(c rune) bool {
+		return unicode.IsSpace(c) || (c == ',')
+	})
+	handler, ok := ruler[rep[0]]
+	if !ok {
+		return "", fmt.Errorf("некорректный ключ %s", rep[0])
+	}
+	rep = rep[1:]
+	if len(rep) == 0 {
+		rep = []string{}
+	}
+	nextTime, err := handler(start, rep)
+
+	if err != nil {
+		return "", fmt.Errorf("ошибка вычисления даты, %w", err)
+	}
+
+	nt0, _ := time.Parse(formatDateTime, nextTime)
+	now0, _ := time.Parse(formatDateTime, now)
+	if nt0.After(now0) {
+		return nextTime, nil
+	}
+	return NextDate(nextTime, now, repeat)
+}
