d06c3a656ff2453b614aff0956467845ac2437d3
Dmitriy Shulgin <dshulgin@mail.ru>
2024-05-10 14:57:28 +0300

    nextdate and utility functions refactored

--- internal/nextdate/nextdate.go
+++ internal/nextdate/nextdate.go
@@ -9,47 +9,196 @@ import (
 )
 
 const (
+	minDays        = 1
 	maxDays        = 400
+	stepYears      = 1
+	weekMonday     = 1
+	weekSunday     = 7
 	formatDateTime = "20060102"
 )
 
+// Тип функции-обработчика для ключей d, w, m, y и пустого значения repeat ("")
 type Handler func(start string, params []string) (string, error)
 
-// допустимо только одно значение < 400
+var ruler = map[string]Handler{
+	"d": repeatInDays,
+	"y": repeatInYears,
+	"w": repeatInWeeks,
+	"m": repeatInMonths,
+	"":  repeatIsEmpty,
+}
+
+// Вычисляет ближайщую дату наступления события.
+// start - текущая дата наступления события
+// now - сегодняшная (на момент вызова функции) дата
+// repeat - условия назначения ближайшей даты наступления события
+func NextDate(start string, now string, repeat string) (string, error) {
+
+	//текущая дата не задана, ближайшей датой считается сегодняшняя дата
+	if len(start) == 0 {
+		return now, nil
+	}
+
+	// условия повторения не определены
+	if len(repeat) == 0 {
+		handler := ruler[""]
+		nextTime, err := handler(start, nil)
+		if err != nil {
+			return "", err
+		}
+		return nextTime, nil
+	}
+
+	rep := strings.FieldsFunc(repeat, func(c rune) bool {
+		return unicode.IsSpace(c) || (c == ',')
+	})
+
+	// допустимые ключи повторения: d, y, w, m
+	handler, ok := ruler[rep[0]]
+	if !ok {
+		return "", fmt.Errorf("некорректное значение поля repeat: %s", rep[0])
+	}
+
+	// параметры ключа не заданы. Допустимо для ключа y, для остальных - считается ошибкой.
+	repTail := rep[1:]
+	if len(repTail) == 0 {
+		repTail = []string{}
+	}
+
+	// Вспомогательная функция NextDate0 циклически вычисляет ближайшую дату
+	// используя выбранную функцию-обработчик (handler)
+	return NextDate0(start, now, repTail, handler)
+}
+
+// Циклически вычисляет ближайшую дату наступления события с помощью функции-обработчика handler.
+func NextDate0(
+	start string,
+	now string,
+	params []string,
+	handler func(start0 string, params0 []string) (string, error)) (string, error) {
+
+	var nextTime string
+	var err error
+	nextTime = start
+	for {
+		nextTime, err = handler(nextTime, params)
+		if err != nil {
+			return "", fmt.Errorf("ошибка вычисления даты, %s", err.Error())
+		}
+		nt0, _ := time.Parse(formatDateTime, nextTime)
+		now0, _ := time.Parse(formatDateTime, now)
+		if nt0.After(now0) {
+			break
+		}
+	}
+	return nextTime, nil
+}
+
+// Вычисляет ближайшую дату события, если условие повторения задано как кол-во дней.
+// Правила формирования условия repeat:
+// Формат: "d <число>", где число находится в пределах 1-400
 func repeatInDays(start string, params []string) (string, error) {
-	///fmt.Printf("repeatInDays, start=%v, params=%v\n", start, params)
+
+	// проверка формата
 	if len(params) == 0 {
 		return "", fmt.Errorf("отсутствует значение для ключа d")
 	}
+
+	// проверка предельных значений ключа
 	days, err := strconv.Atoi(params[0])
-	//fmt.Printf("repeatInDays, days=%v\n", days)
 	if err != nil {
 		return "", fmt.Errorf("некорректное значение аргумента для ключа d: %s", params[0])
 	}
-	if max(days, maxDays) == days {
-		return "", fmt.Errorf("значение аргумента для ключа d превышает 400")
+	if days < minDays || days > maxDays {
+		return "", fmt.Errorf("недопустимое количество дней для ключа d %d (%d-%d))", days, minDays, maxDays)
 	}
+
 	start0, err := time.Parse(formatDateTime, start)
 	if err != nil {
 		return "", fmt.Errorf("ошибка преобразования даты %s", start)
 	}
+
 	nextTime := start0.AddDate(0, 0, days)
-	//fmt.Printf("repeatInDays, nextTime=%v\n", nextTime)
+
 	return nextTime.Format(formatDateTime), nil
 }
 
+// Вычисляет ближайшую дату события, если задача выполняется ежегодно.
+// Выполнение задачи переносится на один год вперед.
+// Правила формирования условия repeat:
+// Формат: "y"
 func repeatInYears(start string, params []string) (string, error) {
+
 	start0, err := time.Parse(formatDateTime, start)
 	if err != nil {
 		return "", fmt.Errorf("ошибка преобразования даты %s", start)
 	}
-	nextTime := start0.AddDate(1, 0, 0)
-	return nextTime.Format(formatDateTime), nil
 
+	nextTime := start0.AddDate(stepYears, 0, 0)
+
+	return nextTime.Format(formatDateTime), nil
 }
 
+// Вычисляет ближайщую дату события, если задача назначена в указанные дни недели, ключ w.
+// Формат: "w <число>,<число>", где 1-понедельник, 7-воскресенье
+// Пример:
 func repeatInWeeks(start string, params []string) (string, error) {
 	return "", fmt.Errorf("неподдерживаемый формат w")
+
+	// // проверка формата
+	// if len(params) == 0 {
+	// 	return "", fmt.Errorf("отсутствует значение для ключа w")
+	// }
+	// // проверка предельных значений
+	// for _, par := range params {
+
+	// 	day, err := strconv.Atoi(par)
+	// 	if err != nil {
+	// 		return "", fmt.Errorf("некорректное значение аргумента для ключа w %d", day)
+	// 	}
+
+	// 	if day < weekMonday || day > weekSunday {
+	// 		return "", fmt.Errorf("некорректное значение аргумента для ключа w %d", day)
+	// 	}
+	// }
+
+	// // какой день недели соотв start
+	// start0, err := time.Parse(formatDateTime, start)
+	// if err != nil {
+	// 	return "", fmt.Errorf("ошибка преобразования даты %s", start)
+	// }
+	// startWeekDay := start0.Weekday()
+	// if startWeekDay == time.Sunday {
+	// 	startWeekDay = weekSunday
+	// }
+	// fmt.Printf("%s is weekday %d\n", start, startWeekDay)
+
+	// sort.Strings(params)
+
+	// for _, par := range params {
+	// 	day, _ := strconv.Atoi(par)
+
+	// 	if day > int(startWeekDay) {
+	// 		return start0.AddDate(0, 0, day-int(startWeekDay)).Format(formatDateTime), nil
+	// 	}
+	// }
+	// dd, _ := strconv.Atoi(params[0])
+	// if dd == int(startWeekDay) {
+	// 	return start0.AddDate(0, 0, 6).Format(formatDateTime), nil
+	// }
+	// count := 7 - int(startWeekDay) + dd
+	// return start0.AddDate(0, 0, count).Format(formatDateTime), nil
+
+	// //задача перносится на ближайший день недели, который наступит раньше
+
+	// // fmt.Printf("count=%d\n", count)
+
+	// // nextTime := start0.AddDate(0, 0, count)
+
+	// // fmt.Printf("nextTime is %s\n", nextTime.Format(formatDateTime))
+
+	// // return nextTime.Format(formatDateTime), nil
+
 }
 
 func repeatInMonths(start string, params []string) (string, error) {
@@ -69,125 +218,3 @@ func repeatIsEmpty(start string, params []string) (string, error) {
 	}
 	return start, nil
 }
-
-var ruler = map[string]Handler{
-	"d": repeatInDays,
-	"y": repeatInYears,
-	"w": repeatInWeeks,
-	"m": repeatInMonths,
-	"":  repeatIsEmpty,
-}
-
-/*
-3. date >= now, repeat == empty => date
-date >= now, repeat => nextdate
-2. date < now, repeat is empty => now
-date < now, repeat => nextdate
-1. date is empty, repeat is empty => now
-1. date is empty, repeat => now
-*/
-func NextDate0(start string, now string, params []string, handler func(start0 string, params0 []string) (string, error)) (string, error) {
-	// nextTime, err := handler(start, params)
-	// if err != nil {
-	// 	return "", fmt.Errorf("ошибка вычисления даты, %w", err)
-	// }
-	// nt0, _ := time.Parse(formatDateTime, nextTime)
-	// now0, _ := time.Parse(formatDateTime, now)
-	// if nt0.After(now0) {
-	// 	return nextTime, nil
-	// }
-	// return NextDate0(nextTime, now, params, handler)
-	var nextTime string
-	var err error
-	nextTime = start
-	for {
-		nextTime, err = handler(nextTime, params)
-		if err != nil {
-			return "", fmt.Errorf("ошибка вычисления даты, %w", err)
-		}
-		nt0, _ := time.Parse(formatDateTime, nextTime)
-		now0, _ := time.Parse(formatDateTime, now)
-		if nt0.After(now0) {
-			break
-		}
-	}
-	return nextTime, nil
-}
-
-func NextDate(start string, now string, repeat string) (string, error) {
-	if len(start) == 0 {
-		return now, nil
-		//n0, _ := time.Parse("20060102", now)
-
-		//return n0.AddDate(0, 0, 1).Format("20060102"), nil
-	}
-	// startDate, err := time.Parse(formatDateTime, start)
-	// if err != nil {
-	// 	return "", fmt.Errorf("некорректное значение поля date, %w", err)
-	// }
-	// nowDate, err := time.Parse(formatDateTime, now)
-	// if err != nil {
-	// 	return "", fmt.Errorf("некорректное значение аргумента now, %w", err)
-	// }
-	// if startDate.Before(nowDate) {
-	// 	if len(repeat) == 0 {
-	// 		return now, nil
-	// 	}
-	// }
-	if len(repeat) == 0 {
-		handler := ruler[""]
-		nextTime, err := handler(start, nil)
-		if err != nil {
-			return "", err
-		}
-		return nextTime, nil
-	}
-	rep := strings.FieldsFunc(repeat, func(c rune) bool {
-		return unicode.IsSpace(c) || (c == ',')
-	})
-	handler, ok := ruler[rep[0]]
-	if !ok {
-		return "", fmt.Errorf("некорректное значение поля repeat: %s", rep[0])
-	}
-	repTail := rep[1:]
-	if len(repTail) == 0 {
-		repTail = []string{}
-	}
-	return NextDate0(start, now, repTail, handler)
-
-	// nextTime, err := handler(start, repTail)
-	// if err != nil {
-	// 	return "", fmt.Errorf("ошибка вычисления даты, %w", err)
-	// }
-	// nt0, _ := time.Parse(formatDateTime, nextTime)
-	// now0, _ := time.Parse(formatDateTime, now)
-	// if nt0.After(now0) {
-	// 	return nextTime, nil
-	// }
-	// return NextDate(nextTime, now, repeat)
-}
-
-// func NextDate(start string, now string, repeat string) (string, error) {
-// 	rep := strings.FieldsFunc(repeat, func(c rune) bool {
-// 		return unicode.IsSpace(c) || (c == ',')
-// 	})
-// 	handler, ok := ruler[rep[0]]
-// 	if !ok {
-// 		return "", fmt.Errorf("некорректное значение поля repeat: %s", rep[0])
-// 	}
-// 	rep = rep[1:]
-// 	if len(rep) == 0 {
-// 		rep = []string{}
-// 	}
-// 	nextTime, err := handler(start, rep)
-// 	if err != nil {
-// 		return "", fmt.Errorf("ошибка вычисления даты, %w", err)
-// 	}
-
-// 	nt0, _ := time.Parse(formatDateTime, nextTime)
-// 	now0, _ := time.Parse(formatDateTime, now)
-// 	if nt0.After(now0) {
-// 		return nextTime, nil
-// 	}
-// 	return NextDate(nextTime, now, repeat)
-// }
